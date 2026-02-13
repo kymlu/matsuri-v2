5a182b95c009f04d783889cf848fb3f80c18963c
Katie <kymlu.4@gmail.com>
2026-02-14 01:10:03 +0900

    clean up mtr export

--- src/lib/helpers/exportHelper.ts
+++ src/lib/helpers/exportHelper.ts
@@ -9,17 +9,15 @@ export function exportToMtr (
   choreo: Choreo,
   filename?: string,
 ) {
-const blob = new Blob([JSON.stringify(choreo)], { type: "application/json" });
+  const blob = new Blob([JSON.stringify(choreo)], { type: "application/json" });
   const url = URL.createObjectURL(blob);
 
   const link = document.createElement("a");
   link.href = url;
   link.download = (filename ?? choreo.name) + ".mtr";
 
-  document.body.appendChild(link);
   link.click();
 
-  document.body.removeChild(link);
   URL.revokeObjectURL(url);
 }
 
