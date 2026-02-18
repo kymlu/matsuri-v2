eca9d5ca59ad99bd6b2214ebb6a019292fd4373d
Katie <kymlu.4@gmail.com>
2026-02-19 02:24:31 +0900

    reset export dialog on complete

--- src/components/dialogs/ExportDialog.tsx
+++ src/components/dialogs/ExportDialog.tsx
@@ -64,6 +64,7 @@ export default function ExportDialog({
         onClose={() => {
           onClose();
           setFollowingId("");
+          setStep("prep");
         }}
         isActionButtonDisabled={isExportNameValid}
       >
