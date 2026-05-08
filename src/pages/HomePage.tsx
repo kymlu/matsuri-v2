7a9246a4e14984639b09be8ceb4a2247b79f3f95
Katie <kymlu.4@gmail.com>
2026-05-08 23:17:36 +0900

    don't show dates if event is not entered

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -718,7 +718,7 @@ function EventSection({
           {event.event?.length === 0 ? "イベント不明" : event.event}
         </div>
         {
-          event.startDate &&
+          (event.startDate || event.endDate) && (event.event?.length ?? 0) === 0 &&
           <div className="p-0 m-0 text-sm font-bold text-gray-600">
             {formatDateRange(event.startDate, event.endDate, false)}
           </div>
