cb85e7ec9bc1afa5a6504d720769c7e66a381ac3
Katie <kymlu.4@gmail.com>
2026-05-21 23:40:50 +0900

    change default snap point for bottom drawer

--- src/components/basic/BottomDrawer.tsx
+++ src/components/basic/BottomDrawer.tsx
@@ -22,7 +22,7 @@ export default function BottomDrawer ({
     <Drawer.Root
       open={open}
       onOpenChange={(open: boolean) => { setOpen(open); }}
-      defaultSnapPoint={"84px"}
+      defaultSnapPoint={0.2}
       snapPoints={["84px", 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]}
       swipeDirection="down"
       modal={false}
