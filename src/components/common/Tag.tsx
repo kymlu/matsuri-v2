ffddfa19db2efd5627d57a79b8dfbbc1a3512a00
Katie <kymlu.4@gmail.com>
2026-05-22 02:25:53 +0900

    make tag a component

---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,34 @@
+import classNames from "classnames"
+import Icon from "../basic/Icon"
+
+type TagProps = {
+  type: "primary" | "filled" | "grey",
+  text: string,
+  icon?: string,
+  compact?: boolean,
+}
+
+export function Tag ({
+  icon, text, type, compact
+}: TagProps) {
+  var tagClasses = classNames("w-min font-semibold flex items-center gap-0.5 border rounded-md text-nowrap",
+    {
+      "bg-primary text-white border-primary": type === "filled",
+      "bg-transparent text-primary border-primary": type === "primary",
+      "bg-transparent text-gray-600 border-gray-600": type === "grey",
+      "px-0.5 py-0 text-xs": compact === true,
+      "px-1 py-0.5 text-sm ": compact !== true,
+    }
+  )
+  
+  return <div className={tagClasses}>
+    <span>{ text }</span>
+    {
+      icon &&
+      <Icon
+        src={icon}
+        colour={type === "filled" ? "white" : type === "grey" ? "grey" : "primary"}
+        size="xs"/>
+    }
+  </div>
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -32,6 +32,7 @@ import AbsentDancersWarningDialog from "../components/dialogs/AbsentDancersWarni
 import BeginnersDialog from "../components/dialogs/BeginnersDialog"
 import EditEventInfoDialog from "../components/dialogs/EditEventInfoDialog"
 import SiteInfoDialog from "../components/dialogs/SiteInfoDialog"
+import { Tag } from "../components/common/Tag"
 
 type HomePageProps = {
   buildInfo?: string,
@@ -777,36 +778,19 @@ function EventSection({
                     }
                     {
                       choreo.status === "upToDate" &&
-                      <div className="px-1 py-0.5 text-sm font-semibold flex items-center gap-0.5 border rounded-md text-primary border-primary text-nowrap">
-                        {
-                          choreo.version &&
-                          <span>v{choreo.version}</span>
-                        }
-                      </div>
+                      <Tag type="primary" text={`v${choreo.version}`}/>
                     }
                     {
                       choreo.status === "syncRequired" &&
-                      <div className="px-1 py-0.5 flex items-center gap-0.5 text-sm font-semibold text-white border rounded-md text-nowrap bg-primary border-primary">
-                        {
-                          choreo.version &&
-                          <span>v{choreo.version}</span>
-                        }
-                        <Icon src={ICON.warning} colour="white" size="xs"/>
-                      </div>
+                      <Tag type="filled" text={`v${choreo.version}`} icon={ICON.warning}/>
                     }
                     {
                       choreo.status === "edited" &&
-                      <div className="px-1 py-0.5 text-sm font-semibold flex items-center gap-0.5 border rounded-md text-primary border-primary text-nowrap">
-                        {
-                          choreo.version &&
-                          <span>v{choreo.version}</span>
-                        }
-                        <Icon src={ICON.edit} colour="primary" size="xs"/>
-                      </div>
+                      <Tag type="primary" text={`v${choreo.version}`} icon={ICON.edit}/>
                     }
                     {
                       (choreo.status === "localOnly") &&
-                      <div className="px-1 py-0.5 text-sm text-gray-600 border border-gray-600 rounded-md text-nowrap">未公開</div>
+                      <Tag type="grey" text="未公開"/>
                     }
                     <Dialog.Trigger id={choreo.id} payload={choreo} handle={optionsDialog} onClick={(e) => {
                       e.stopPropagation();
