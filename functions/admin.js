80e835d4bb5357ce0a8b19e1cfffced10c6a3bf3
Katie <kymlu.4@gmail.com>
2026-05-30 18:16:48 +0900

    fix admin page

--- functions/admin.js
+++ functions/admin.js
@@ -1,6 +1,10 @@
-export function onRequest() {
-  const url = new URL(context.request.url);
-  url.pathname = "/";
-  url.searchParams.set("loggedIn", "true");
-  return Response.redirect(url.toString(), 302);
+export async function onRequest(context) {
+  try {
+    const url = new URL(context.request.url);
+    url.pathname = "/";
+    url.searchParams.set("loggedIn", "true");
+    return Response.redirect(url.toString(), 302);
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
 }
