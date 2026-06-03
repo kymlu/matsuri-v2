dcdad02ea8b1ccadd2a07b5e8e4c0e3991a378ba
Katie <kymlu.4@gmail.com>
2026-06-03 20:19:05 +0900

    add spinner for login dialog

--- src/components/dialogs/LoginDialog.tsx
+++ src/components/dialogs/LoginDialog.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import BaseEditDialog from "./BaseEditDialog";
+import { Oval } from "react-loader-spinner";
+import { colorPalette } from "../../lib/consts/colors";
 
 export default function LoginDialog() {
   const checkAuth = async () => {
-    window.location.href = "/admin";
+    if (!isProcessing) {
+      setIsProcessing(true);
+      window.location.href = "/admin";
+    }
   };
+
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
 
   return <BaseEditDialog
     title="管理者ログイン"
@@ -12,5 +20,12 @@ export default function LoginDialog() {
     onSubmit={checkAuth}
   >
     隊列表を公開するには、メール認証が必要です。
+    {
+      isProcessing &&
+      <Oval
+        wrapperClass="mt-4 justify-self-center"
+        color={colorPalette.primary}
+        secondaryColor={colorPalette.rainbow.red[2]}/>
+    }
   </BaseEditDialog>
 }
