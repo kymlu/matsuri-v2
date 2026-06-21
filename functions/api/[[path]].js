cd92ed887e13365aa407612bedda25fd18dbeed6
Katie <kymlu.4@gmail.com>
2026-06-22 00:17:41 +0900

    fix passing the cookie

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,12 +1,4 @@
 export async function onRequest(context) {
-  const cookie = context.request.headers.get("cookie") ?? "";
-  const token = cookie.split(";")
-    .find(c => c.trim().startsWith("CF_Authorization="))
-    ?.split("=")[1]?.trim();
-
-  console.debug("cookie", cookie);
-  console.debug("token", token);
-
   const originalUrl = new URL(context.request.url);
   const workerUrl = `https://${context.env.API_URL}${originalUrl.pathname}${originalUrl.search}`;
 
@@ -14,7 +6,7 @@ export async function onRequest(context) {
     method: context.request.method,
     headers: {
       "Content-Type": "application/json",
-      "X-Matsuri-Access-Token": token ?? "",
+      "cookie": context.request.headers.get("cookie") ?? "",
     },
     body: context.request.method !== "GET" ? context.request.body : undefined,
   });
