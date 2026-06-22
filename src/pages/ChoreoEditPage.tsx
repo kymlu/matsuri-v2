9730df6154d66a6deb0858ddeea3d865a24a2a2f
Katie <kymlu.4@gmail.com>
2026-06-22 23:57:45 +0900

    reload page after publish

--- src/pages/ChoreoEditPage.tsx
+++ src/pages/ChoreoEditPage.tsx
@@ -1214,7 +1214,7 @@ export default function ChoreoEditPage(props: {
         <BaseEditDialog
           title="公開成功"
           hasX={false}
-          onSubmit={props.goToHomePage}
+          onSubmit={() => {window.location.reload()}}
           actionButtonText="ホーム画面へ戻る"
           showCloseButton={false}>
           隊列表の公開が完了しました。
