baf4df313d4cf190b2a028e68b0d4709d772e338
Katherine Lu <50158071+kymlu@users.noreply.github.com>
2026-07-24 16:50:09 +0900

    Update MovementEditLayer.tsx
    Improve hit box on midpoints

--- src/components/grid/layers/MovementEditLayer.tsx
+++ src/components/grid/layers/MovementEditLayer.tsx
@@ -90,6 +90,7 @@ const MovementEditLayer = memo(function MovementEditLayer({
             strokeEnabled
             stroke={colorPalette.primary}
             strokeWidth={2}
+            hitStrokeWidth={METER_PX/2}
             fill={colorPalette.white}
             fillEnabled
             radius={METER_PX/5}
