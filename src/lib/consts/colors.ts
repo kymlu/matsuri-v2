b75abb008d0df1fbddb85aea61c041817bcc141c
Katie <kymlu.4@gmail.com>
2026-08-04 22:24:27 +0900

    change in use colour to be easier on the eyes

--- src/lib/consts/colors.ts
+++ src/lib/consts/colors.ts
@@ -23,7 +23,7 @@ export const colorPalette = {
   lightGrey: "#CDCDCD",
   paleGrey: "#D4D4D4",
   primary: "#AB1010",
-  inUseColour: "#00FDE0",
+  inUseColour: "#0000FF",
 
   actionOutlineColours: [
     "#F3C300", "#875692", "#F38400", "#A1CAF1",
